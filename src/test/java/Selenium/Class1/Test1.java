5c40f6aa870a12c8fe2dada670c1b74a44ff05b2
Salma <salmahossain52@gmail.com>
2023-01-04 11:37:04 -0600

    first commit

---
+++ src/test/java/Selenium/Class1/Test1.java
@@ -0,0 +1,15 @@
+package Selenium.Class1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://demoqa.com/text-box");
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
